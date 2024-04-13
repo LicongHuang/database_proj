21542497a5de1e5c87cbbfcaed70a426067b7748
Chow Yuan Jing <zcyuanjing@gmail.com>
2024-04-13 18:44:37 +0800

    Added procedures 3 and 4
    Procedure 3 passes tests but not procedure 4.

--- code/p.sql
+++ code/p.sql
@@ -26,8 +26,7 @@ INSERT ON Hires
 FOR EACH ROW EXECUTE FUNCTION check_double_booked();
 
 
-CREATE OR REPLACE FUNCTION check_car_double_booking()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_car_double_booking() RETURNS TRIGGER AS $$
 DECLARE
   new_sdate DATE;
   new_days INT;
@@ -46,19 +45,21 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
 CREATE TRIGGER prevent_car_double_booking
-BEFORE INSERT ON Assigns
+BEFORE
+INSERT ON Assigns
 FOR EACH ROW EXECUTE PROCEDURE check_car_double_booking();
 
 -- During handover the employee must be located in the same location the booking is for
-CREATE OR REPLACE FUNCTION check_employee_location()
-RETURNS TRIGGER AS $$
+
+CREATE OR REPLACE FUNCTION check_employee_location() RETURNS TRIGGER AS $$
 BEGIN
   IF NOT EXISTS (
-    SELECT 1 
+    SELECT 1
     FROM Employees E
     JOIN Bookings B ON E.zip = B.zip
-    WHERE eid = NEW.eid 
+    WHERE eid = NEW.eid
     AND E.zip = B.zip
   ) THEN
     RAISE EXCEPTION 'Employee must be located in the same location as the booking';
@@ -67,18 +68,18 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER prevent_employee_location
-BEFORE INSERT ON Handover
-FOR EACH ROW EXECUTE FUNCTION check_employee_location();
 
+CREATE TRIGGER prevent_employee_location
+BEFORE
+INSERT ON Handover
+FOR EACH ROW EXECUTE FUNCTION check_employee_location();
 
 -- The car assigned to the booking must be for the car models for the booking
 -- Trigger on insertion into Assigns
 -- When customer initiates a booking (id by bid), the customer selects a car model (identified by (brand, model))
 -- When a car (CarDetails indentified by plate) is assigned to a booking after the booking is initiated, it must have the same (brand, model) as the booking
 
-CREATE OR REPLACE FUNCTION check_car_model()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_car_model() RETURNS TRIGGER AS $$
 BEGIN
   IF NOT EXISTS (
     SELECT 1
@@ -94,14 +95,15 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
 CREATE TRIGGER prevent_car_model
-BEFORE INSERT ON Assigns
+BEFORE
+INSERT ON Assigns
 FOR EACH ROW EXECUTE FUNCTION check_car_model();
 
 -- Driver must be hired within the start date and end date of a booking
 
-CREATE OR REPLACE FUNCTION check_driver_hire()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_driver_hire() RETURNS TRIGGER AS $$
 BEGIN
   IF NOT EXISTS (
     SELECT 1
@@ -117,23 +119,23 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
 CREATE TRIGGER prevent_driver_hire
-BEFORE INSERT ON Hires
+BEFORE
+INSERT ON Hires
 FOR EACH ROW EXECUTE FUNCTION check_driver_hire();
 
 
-CREATE OR REPLACE PROCEDURE add_employees (
-  eids INT[], enames TEXT[], ephones INT[], zips INT[], pdvls TEXT[]
-) AS $$
+CREATE OR REPLACE PROCEDURE add_employees (eids INT[], enames TEXT[], ephones INT[], zips INT[], pdvls TEXT[]) AS $$
 DECLARE
   i INT;
 BEGIN
   FOR i IN 1..array_length(eids, 1) LOOP
-    INSERT INTO Employees (eid, ename, ephone, zip) 
+    INSERT INTO Employees (eid, ename, ephone, zip)
     VALUES (eids[i], enames[i], ephones[i], zips[i]);
 
     IF pdvls[i] IS NOT NULL THEN
-      INSERT INTO Drivers (eid, pdvl) 
+      INSERT INTO Drivers (eid, pdvl)
       VALUES (eids[i], pdvls[i]);
     END IF;
   END LOOP;
@@ -141,11 +143,8 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- PROCEDURE 2
-CREATE OR REPLACE PROCEDURE add_car (
-  brand   TEXT   , model  TEXT   , capacity INT  ,
-  deposit NUMERIC, daily  NUMERIC,
-  plates  TEXT[] , colors TEXT[] , pyears   INT[], zips INT[]
-) AS $$
+
+CREATE OR REPLACE PROCEDURE add_car (brand TEXT , model TEXT , capacity INT , deposit NUMERIC, daily NUMERIC, plates TEXT[] , colors TEXT[] , pyears INT[], zips INT[]) AS $$
 DECLARE
   i INT;
 BEGIN
@@ -162,45 +161,104 @@ $$ LANGUAGE plpgsql;
 
 -- PROCEDURE 3
 
+CREATE OR REPLACE PROCEDURE return_car (bid INT, eid INT) AS $$
+-- add declarations here
+DECLARE
+  cost NUMERIC;
+  days INT;
+  daily NUMERIC;
+  cbrand TEXT;
+  cmodel TEXT;
+  ccnum TEXT;
+  deposit NUMERIC;
+BEGIN
+  -- CHECKS
+  IF NOT EXISTS (
+    SELECT 1
+    FROM Bookings B
+    WHERE B.bid = return_car.bid
+  ) THEN
+    RAISE EXCEPTION 'No booking with that bid exists';
+  END IF;
 
--- CREATE OR REPLACE PROCEDURE return_car (
---   bid INT, eid INT
--- ) AS $$
--- DECLARE
---   plate TEXT; cost NUMERIC;
---   ccnum TEXT; days INT;
---   deposit NUMERIC; daily NUMERIC;
--- BEGIN
---   SELECT A.plate, B.days, B.deposit, B.daily, BK.ccnum 
---   INTO plate, days, deposit, daily, ccnum
---   FROM Assigns A
---   JOIN Bookings B ON A.bid = B.bid
---   JOIN Bookings BK ON A.bid = BK.bid
---   WHERE A.bid = bid;
--- 
---   cost := daily * days + deposit ;
--- 
--- 
---   INSERT INTO Returned (bid, eid, ccnum, cost)
---   VALUES (bid, eid, plate, ccnum, cost);
--- 
--- END;
--- $$ LANGUAGE plpgsql;
+  IF NOT EXISTS (
+    SELECT 1
+    FROM Employees E
+    WHERE E.eid = return_car.eid
+  ) THEN
+    RAISE EXCEPTION 'No employee with that eid exists';
+  END IF;
 
+  -- LOGIC
+  SELECT B.brand, B.model, B.days, B.ccnum INTO cbrand, cmodel, days, ccnum FROM Bookings B WHERE B.bid = return_car.bid;
+  SELECT cm.daily, cm.deposit INTO daily, deposit FROM CarModels cm
+  WHERE cm.brand = cbrand
+  AND cm.model = cmodel;
+
+  -- your code here
+  cost := daily * days - deposit;
+  INSERT INTO Returned (bid, eid, ccnum, cost)
+  VALUES (bid, eid, ccnum, cost);
+  IF cost > 0 AND ccnum IS NULL THEN
+    RAISE EXCEPTION 'No ccnum provided';
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- PROCEDURE 4
+
+CREATE OR REPLACE PROCEDURE auto_assign () AS $$
+-- add declarations here
+DECLARE
+  c CURSOR FOR (
+    SELECT *
+    FROM Bookings b
+    WHERE b.bid NOT IN (
+      SELECT bid
+      FROM Assigns
+    )
+    ORDER BY b.bid ASC
+  );
+  r RECORD;
+  brand TEXT;
+  model TEXT;
+  zip INT;
+  sdate DATE;
+  days INT;
+  plate TEXT;
+BEGIN
+  OPEN c;
+  LOOP
+    FETCH c INTO r;
+    EXIT WHEN NOT FOUND;
+    SELECT cd.plate INTO plate
+    FROM CarDetails cd
+    WHERE cd.brand = r.brand
+    AND cd.model = r.model
+    AND cd.zip = r.zip
+    AND NOT EXISTS (
+      SELECT 1
+      FROM Bookings b, Assigns a
+      WHERE b.bid = a.bid
+      AND NOT (b.sdate, b.sdate + b.days) OVERLAPS (r.sdate, r.sdate + r.days)
+    )
+    ORDER BY cd.plate ASC
+    LIMIT 1;
+
+    IF plate IS NOT NULL THEN
+      INSERT INTO Assigns (bid, plate) VALUES (r.bid, plate);
+    END IF;
+  END LOOP;
+  -- your code here
+END;
+$$ LANGUAGE plpgsql;
 
 -- Find all bookings without assigned car details
 -- Sort the bookings found in Step a in ascending order of the bid
 -- Sort all car details in ascending order of plate
 -- Starting from the smallest booking bid, assing the car with the smallest plate that satisfies it booking requirement
-
--- Recap booking requirements:
+ -- Recap booking requirements:
 -- 1. The brand and model match
 -- 2. The location identified by zip match
 -- 3. The car is not double booked
 -- -- Note that we may auto assign the same car multiple times as we assume the car will be returned on time
-
-
-
-
-
-
